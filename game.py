09f63ec31ee85de81817384f3e4da902d65a817b
Minhaj Ap <minhajap00@gmail.com>
2024-11-03 10:29:21 +0530

    modulerised the code

--- game.py
+++ game.py
@@ -1,37 +1,44 @@
 import random
 
-options = ["rock", "paper", "scissor"]
-userScore = 0
-botScore = 0
-attempts = 6
-while attempts > 0:
-    botOption = options[random.randint(0, 2)]
-    userOption = input("Choose your option(rock,paper,sccissor): ").lower()
-    attempts -= 1
-    print(f"Bot choosed: {botOption}")
-    if (
-        (userOption == "rock" and botOption == "scissor")
-        or (userOption == "scissor" and botOption == "paper")
-        or (userOption == "paper" and botOption == "rock")
-    ):
-        print("You gained a score")
-        userScore += 1
-    elif (
-        (botOption == "rock" and userOption == "scissor")
-        or (botOption == "scissor" and userOption == "paper")
-        or (botOption == "paper" and userOption == "rock")
-    ):
-        print("Bot gained a score")
-        botScore += 1
-    elif userOption not in options:
-        print("Invalid input")
-        attempts+=1
+
+def gamePlay():
+    options = ["rock", "paper", "scissor"]
+    userScore = 0
+    botScore = 0
+    attempts = 6
+    while attempts > 0:
+        botOption = options[random.randint(0, 2)]
+        userOption = input("Choose your option(rock,paper,sccissor): ").lower()
+        attempts -= 1
+        print(f"Bot choosed: {botOption}")
+        if userOption not in options:
+            print("Invalid input")
+            attempts += 1
+        elif userOption == botOption:
+            print("It's a tie")
+        else:
+            userScore += scoreChooser(userOption, botOption, "user")
+            botScore += scoreChooser(botOption, userOption, "bot")
+
+    if botScore < userScore:
+        print("YOU WON!!!")
+    elif userScore < botScore:
+        print("YOU LOST")
     else:
-        print("It's a tie")
-if botScore < userScore:
-    print("YOU WON!!!")
-elif userScore < botScore:
-    print("YOU LOST")
-else:
-    print("IT'S A TIE")
-print(f"Your score:{userScore}\n Bot score:{botScore}")
+        print("IT'S A TIE")
+    print(f"Your score:{userScore}\n Bot score:{botScore}")
+
+
+def scoreChooser(option1, option2, player):
+    if (
+        (option1 == "rock" and option2 == "scissor")
+        or (option1 == "scissor" and option2 == "paper")
+        or (option1 == "paper" and option2 == "rock")
+    ):
+        print(f"You {"lost"if player=="bot" else "gained"} a score")
+        return 1
+    else:
+        return 0
+
+
+gamePlay()
